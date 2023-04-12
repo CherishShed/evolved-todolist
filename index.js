c1c53e868f921742ca78623a083626ea1bbde0dd
CherishShed <c.shed2000@gmail.com>
2023-04-12 14:42:32 +0100

    original commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "todolist"
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    const tasks = [];
+    con.connect((err) => {
+        if (err) throw err;
+        console.log("Connected");
+        var stmt = 'select taskId, task, time from taskData';
+        con.query(stmt, (err, result) => {
+            if (err) throw err;
+            tasks.push(...result);
+            res.render("index", { root: __dirname, tasks })
+        })
+    })
+})
+app.post("/", (req, res) => {
+    var taskDet = req.body.taskDets;
+    var timeToDo = new Date(req.body.time).toISOString();
+    timeToDo = timeToDo.replace(/T|\.000Z/g, " ");
+    timeToDo = timeToDo.trim();
+    var stmt = `INSERT INTO taskData (task, time) VALUES ('${taskDet}', '${timeToDo}')`;
+    con.query(stmt, (err, result) => {
+        if (err) throw err;
+    })
+    res.redirect("/");
+})
+
+
+app.delete("/:id", (req, res) => {
+    const idToDelete = req.params.id;
+    console.log(idToDelete)
+        // Execute the DELETE query on the database
+        ;
+    con.query(`DELETE FROM taskData WHERE taskId = ${idToDelete}`, (error, results, fields) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error deleting record from database');
+        } else {
+            res.status(200).send(`Record with ID ${idToDelete} deleted successfully`);
+        }
+    });
+})
+
+app.listen(8081, () => {
+    console.log("Server has started at port 8081")
+})
+
---
+++ public/dom.js
@@ -0,0 +1,154 @@
+var today = new Date(Date.now())
+var meridian = "";
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var dateNum = String(today.getDate())
+var prefix = "";
+if ($(".task-count").text() == "NaN") {
+    var numOfTasks = 0;
+} else {
+    var numOfTasks = parseInt($(".task-count").text());
+}
+var iconElement = "<i class= fa-solid fa-check></i>"
+function checkMeridian() {
+    if (new Date(Date.now()).getHours() > 11) {
+        meridian = "PM";
+    } else {
+        meridian = "AM";
+    }
+}
+
+function changeDate() {
+    $("#dayOfWeek").fadeOut(10);
+    $("#dayOfWeek").text(days[today.getDay()] + ", ");
+    $("#dayOfWeek").fadeIn(1000);
+
+    if (dateNum.charAt(dateNum.length - 1) === "1") {
+        prefix = "st";
+    }
+    else if (dateNum.charAt(dateNum.length - 1) === "2") {
+        prefix = "nd";
+    }
+    else if (dateNum.charAt(dateNum.length - 1) === "3") {
+        prefix = "rd";
+    }
+    else {
+        prefix = "th";
+    }
+
+    $("#date").slideUp(10);
+    $("#date").text(today.getDate() + prefix);
+    $("#date").slideDown(800);
+
+    $("#month").slideUp(10);
+    $("#month").text(months[today.getMonth()] + ", ");
+    $("#month").slideDown(800);
+
+    $("#year").slideUp(10);
+    $("#year").text(today.getFullYear());
+    $("#year").slideDown(800);
+
+    // $(".timeToDo").html("<i class='fa-solid fa-stopwatch'></i> " + (new Date(Date.now()).toUTCString()))
+    $("#clockTime").slideUp(10);
+    $("#clockTime").text(today.getHours() + ":" + today.getMinutes() + " " + meridian);
+    $("#clockTime").slideDown(800);
+    taskCalc(numOfTasks);
+}
+
+function taskCalc(numTasks) {
+    if ($(".task-count").text() == "NaN") {
+        numTasks = 0;
+    }
+    if (numTasks === 1) {
+        $("#taskNum").slideUp(10);
+        $("#taskNum").slideDown(700);
+        $("#taskNum").text(numTasks + " Task")
+    } else {
+        $("#taskNum").slideUp(10);
+        $("#taskNum").slideDown(700);
+        $("#taskNum").text(numTasks + " Tasks")
+    }
+}
+
+changeDate();
+addRemove();
+
+
+$(".addButton").click(function () {
+    $(".form-overlay").show(500);
+})
+
+$(".newTask .close").click(function () {
+    $(".form-overlay").hide(500);
+})
+
+// $(".addNew").click(function () {
+//     // console.log(newTask);
+//     $(".mainList").append(newTask);
+//     storeTask(newTask);
+//     numOfTasks++;
+//     taskCalc(numOfTasks);
+//     addRemove();
+//     $(".newTask").hide(500);
+
+// })
+
+function addRemove() {
+    $(".mainList li").click(function (ev) {
+        $(this.firstChild).toggleClass("icon")
+
+        $(this).toggleClass("checked");
+        // console.log("'" + ev.target);
+    })
+
+    // $("li").append("<span class='close'>X</span>");
+
+
+    $("li").mouseenter(function () {
+        $(this.lastChild).show();
+    }
+    );
+
+    $("li").mouseleave(function () {
+        $(this.lastChild).hide();
+    }
+    );
+
+    $("span.close").click(function (ev) {
+        let happenedTo = ev.target.parentElement;
+        $(this.parentElement).slideUp(500, function () {
+            $(ev.parentElement).css("display", "none");
+            $(happenedTo).remove();
+            storeTask();
+            numOfTasks--
+            if ($(".task-count").text() == "NaN") {
+                numOfTasks = 0;
+            }
+            taskCalc(numOfTasks);
+        });
+
+        const idToDelete = $(this).data('id');
+
+        // Send a DELETE request to the server using Ajax
+        $.ajax({
+            url: `/${idToDelete}`,
+            type: 'DELETE',
+            success: function (result) {
+                console.log(result);
+                // Handle the success case, such as removing the deleted record from the DOM
+            },
+            error: function (error) {
+                console.error(error);
+                // Handle the error case, such as displaying an error message
+            }
+        });
+    })
+
+}
+
+
+setInterval(function () {
+    checkMeridian();
+    let x = new Date(Date.now());
+    $("#clockTime").text(("0" + x.getHours()).slice(-2) + ":" + ("0" + x.getMinutes()).slice(-2) + " " + meridian);
+}, 500);
